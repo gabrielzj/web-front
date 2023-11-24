178659e4d34fb210d22e4de2a08b25884882f3bf
Gabriel Zuin <gabrielzuinj@gmail.com>
2023-11-24 20:35:58 -0300

    update: function limpaLista

--- script/limpaLista.js
+++ script/limpaLista.js
@@ -8,8 +8,8 @@ deleteBtn.addEventListener("click", del);
 function del() {
     list.style.display = 'none';
     localStorage.removeItem("nome");
-    localStorage.removeItem("pass");
-    localStorage.removeItem("pais");
+    localStorage.removeItem("senha");
+    localStorage.removeItem("país");
     localStorage.removeItem("idade");
     localStorage.removeItem("msg");
 }
