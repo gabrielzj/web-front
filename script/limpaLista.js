fa44fb05bddc93a3cdfbf0c2ccd2dcaf506e11f3
Gabriel Zuin <gabrielzuinj@gmail.com>
2023-11-24 20:19:17 -0300

    update: function limpaLista

--- script/limpaLista.js
+++ script/limpaLista.js
@@ -2,11 +2,14 @@
 let list = document.querySelector("#list");
 let deleteBtn = document.querySelector("#delete");
 
+
 deleteBtn.addEventListener("click", del);
 
 function del() {
-  if(list.style.display != 'none')
     list.style.display = 'none';
-  else
-    window.alert('Lista Vazia!');
+    localStorage.removeItem("nome");
+    localStorage.removeItem("pass");
+    localStorage.removeItem("pais");
+    localStorage.removeItem("idade");
+    localStorage.removeItem("msg");
 }
